404f5f5592c308dc21832d203c9ea15eb0ce524a
dorkrepublic <dorkrepublicgamediv@gmail.com>
2015-06-10 22:36:03 -0700

    Worked a small bit on arenas

--- LeOMi/src/com/dorkrepublicgaming/leomi/arena/Arena.java
+++ LeOMi/src/com/dorkrepublicgaming/leomi/arena/Arena.java
@@ -1,5 +1,43 @@
 package com.dorkrepublicgaming.leomi.arena;
 
-public class Arena {
+import org.bukkit.Location;
 
+public class Arena {
+	
+	String name;
+	Location topXY;
+	Location bottomXY;
+	
+	public Arena(String name, Location topXY, Location bottomXY) {
+		this.name = name;
+		this.topXY = topXY;
+		this.bottomXY = bottomXY;
+	}
+	
+	public boolean isInBounds(Location loc) {
+		int gX = loc.getBlockX();
+		int gY = loc.getBlockY();
+		int gZ = loc.getBlockZ();
+		int X1 = a().getBlockX();
+		int Y1 = a().getBlockY();
+		int Z1 = a().getBlockZ();
+		int X2 = b().getBlockX();
+		int Y2 = b().getBlockY();
+		int Z2 = b().getBlockZ();
+		return (d(c(gX, X1), c(gX, X2)) && d(c(gY, Y1), c(gY, Y2)) && d(c(gZ, Z1), c(gZ, Z2)));
+		// Monotonous. Labeling everything by letter saves space. Also, redundant sentences are redundant.
+	}
+	
+	private Location a() {
+		return topXY;
+	}
+	private Location b() {
+		return bottomXY;
+	}
+	private boolean c(int a, int b) {
+		return a <= b;
+	}
+	private boolean d(boolean a, boolean b) {
+		return a == b && b == a;
+	}
 }
--- LeOMi/src/com/dorkrepublicgaming/leomi/arena/ArenaHandler.java
+++ LeOMi/src/com/dorkrepublicgaming/leomi/arena/ArenaHandler.java
@@ -1,5 +1,11 @@
 package com.dorkrepublicgaming.leomi.arena;
 
-public class ArenaHandler {
+import org.bukkit.Location;
 
+public class ArenaHandler {
+	
+	public static Arena createArena(String name, Location topXY, Location bottomXY) {
+		return new Arena(name, topXY, bottomXY);
+	}
+	
 }
